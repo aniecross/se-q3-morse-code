53eeb28f875b94f0f75621d9e7c2fec3f9dfcb6f
Anie Cross <aniecross@aniecross@ymail.com>
2020-08-30 19:20:05 -0500

    initial commit, morse assessment

--- morse.py
+++ morse.py
@@ -9,19 +9,58 @@ Pause between dots and dashes in a character – is 1 time unit long.
 Pause between characters inside a word – is 3 time units long.
 Pause between words – is 7 time units long.
 """
-__author__ = '???'
+__author__ = """Anie Cross with help from instructor demo recordings,
+Group-B discussion topics, Google search, docs.python.org, stackoverflow.com,
+assessment completed with help from Tutor HPost"""
 
 from morse_dict import MORSE_2_ASCII
 
 
 def decode_bits(bits):
-    # your code here
-    return
+    bits = bits.strip("0")
+    unit = 0
+    for bit in bits:
+        if bit != "0":
+            unit += 1
+        else:
+            break
+    # unit now might be 1 unit or 3 units
+    count = 1
+    for i in range(1, len(bits)):
+        if bits[i] == bits[i-1]:
+            count += 1
+        else:
+            if count < unit:
+                unit = count
+                count = 1
+            else:
+                count = 1
+    morse_code = ""
+    words = bits.split("0"*7*unit)
+    for word in words:
+        characters = word.split("0"*3*unit)
+        for character in characters:
+            signs = character.split("0"*unit)
+            for sign in signs:
+                if sign != "":
+                    sign.strip('0')
+                    if sign == "1"*3*unit:
+                        morse_code += "-"
+                    else:
+                        morse_code += "."
+            morse_code += " "
+        morse_code += "  "
+    return morse_code.strip(" ")
 
 
 def decode_morse(morse):
-    # your code here
-    return
+    results = ''
+    for item in morse.split('  '):  # gives word
+        for ltr in item.split(' '):     # gives characters
+            if ltr != '':
+                results += MORSE_2_ASCII[ltr]
+        results += ' '  # this is for space in between words
+    return results.strip(' ')   # strip ending space and beginning
 
 
 if __name__ == '__main__':
